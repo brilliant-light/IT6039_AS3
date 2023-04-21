b0324011eff54f8f40b69c547121a15cab932bf5
brilliant-light <gray.hawes28@gmail.com>
2023-04-21 16:25:19 +1200

    Syntax Update
    Adding the appropriate amount of blank lines from between the score and strike function and onwards.

--- BowlingGame.py
+++ BowlingGame.py
@@ -26,16 +26,24 @@ class BowlingGame:
                 result += self.frameScore(rollIndex)
             rollIndex += 2
             return result
+        
+
 
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
+    
+
     def isSpare(self, rollIndex):
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
+    
+
     def stickeScore(self,rollIndex):
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
+    
 
     def spareScore(self,rollIndex):
         return  10+ self.rolls[rollIndex+2]
+
 
     def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
